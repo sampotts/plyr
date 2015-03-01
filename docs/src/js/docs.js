e73eb87c7e3842dc3c09c5f1c36e5d356e5b6096
Sam Potts <me@sampotts.me>
2015-03-02 01:31:31 +1100

    Fixed domain check

--- docs/src/js/docs.js
+++ docs/src/js/docs.js
@@ -15,8 +15,8 @@ plyr.setup({
 });
 
 // Google analytics 
-// For demo site (http://plyr.io) only
-if(document.domain === "plyr.io") {
+// For demo site (http://[www.]plyr.io) only
+if(document.domain.indexOf("plyr.io") > -1) {
 	(function(i,s,o,g,r,a,m){i.GoogleAnalyticsObject=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
