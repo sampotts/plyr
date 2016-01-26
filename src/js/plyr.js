a285fcc4ec45816d43fa3bfe35c23dd21e7bfc63
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-01-26 17:05:57 +0100

    Check for module.exports as well for CommonJS Modules

--- src/js/plyr.js
+++ src/js/plyr.js
@@ -11,7 +11,7 @@
     'use strict';
     /*global define,module*/
 
-    if (typeof module === 'object') {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
         // Node, CommonJS-like
         module.exports = factory(root, document);
     } else if (typeof define === 'function' && define.amd) {
