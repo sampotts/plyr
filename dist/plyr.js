33a11fb53adff8716935755248e12a1597254233
Sam Potts <sam@potts.es>
2018-05-09 09:50:22 +1000

    v3.3.7

--- dist/plyr.js
+++ dist/plyr.js
@@ -3747,7 +3747,7 @@ var defaults$1 = {
     // Sprite (for icons)
     loadSprite: true,
     iconPrefix: 'plyr',
-    iconUrl: 'https://cdn.plyr.io/3.3.6/plyr.svg',
+    iconUrl: 'https://cdn.plyr.io/3.3.7/plyr.svg',
 
     // Blank video (used to prevent errors on source change)
     blankVideo: 'https://cdn.plyr.io/static/blank.mp4',
