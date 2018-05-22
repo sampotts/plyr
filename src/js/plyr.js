a25a509abe34fae70ae276e6c335d85194e39116
Raccoon <raccoon@Raccoons-MacBook-Pro.local>
2018-05-22 18:06:44 +0800

    fix: IE cannot use remove

--- src/js/plyr.js
+++ src/js/plyr.js
@@ -25,6 +25,14 @@
 }(typeof window !== 'undefined' ? window : this, function(window, document) {
     'use strict';
 
+    (function() {
+        function remove() {
+            this.parentNode && this.parentNode.removeChild(this);
+        }
+        if (!Element.prototype.remove) Element.prototype.remove = remove;
+        if (Text && !Text.prototype.remove) Text.prototype.remove = remove;
+    })();
+
     // Globals
     var scroll = { x: 0, y: 0 },
 
