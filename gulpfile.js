58feff7ae2720cd1a0a058f26379a56be9319f7a
Filip Š <filip.stamcar@hotmail.com>
2018-11-21 16:01:56 +0100

    Add support for building translations

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ const paths = {
         // Source paths
         src: {
             sass: path.join(root, 'src/sass/**/*.scss'),
-            js: path.join(root, 'src/js/**/*.js'),
+            js: path.join(root, 'src/{js,locales}/**/*.js'),
             sprite: path.join(root, 'src/sprite/*.svg'),
         },
 
