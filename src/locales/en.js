ed4af258de7dc7ecc5791ade58d28f4f1e55399c
Filip Š <filip.stamcar@hotmail.com>
2018-11-21 16:00:40 +0100

    Add support for translations (fix sampotts/plyr#1261)

---
+++ src/locales/en.js
@@ -0,0 +1,51 @@
+// ==========================================================================
+// Plyr English translation
+// ==========================================================================
+
+const i18n = {
+  restart: 'Restart',
+  rewind: 'Rewind {seektime}s',
+  play: 'Play',
+  pause: 'Pause',
+  fastForward: 'Forward {seektime}s',
+  seek: 'Seek',
+  seekLabel: '{currentTime} of {duration}',
+  played: 'Played',
+  buffered: 'Buffered',
+  currentTime: 'Current time',
+  duration: 'Duration',
+  volume: 'Volume',
+  mute: 'Mute',
+  unmute: 'Unmute',
+  enableCaptions: 'Enable captions',
+  disableCaptions: 'Disable captions',
+  download: 'Download',
+  enterFullscreen: 'Enter fullscreen',
+  exitFullscreen: 'Exit fullscreen',
+  frameTitle: 'Player for {title}',
+  captions: 'Captions',
+  settings: 'Settings',
+  menuBack: 'Go back to previous menu',
+  speed: 'Speed',
+  normal: 'Normal',
+  quality: 'Quality',
+  loop: 'Loop',
+  start: 'Start',
+  end: 'End',
+  all: 'All',
+  reset: 'Reset',
+  disabled: 'Disabled',
+  enabled: 'Enabled',
+  advertisement: 'Ad',
+  qualityBadge: {
+    '480': 'SD',
+    '576': 'SD',
+    '720': 'HD',
+    '1080': 'HD',
+    '1440': 'HD',
+    '2160': '4K',
+  },
+};
+
+export default i18n;
+
---
+++ src/locales/index.js
@@ -0,0 +1,11 @@
+// ==========================================================================
+// Plyr supported languages
+// ==========================================================================
+
+import en from './en';
+
+const languages = [
+    { code: 'en', name: 'English', i18n: en },
+];
+
+export default languages;
