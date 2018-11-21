ac219bea13705662ebc5666ee1ee8692ff712895
Filip Š <filip.stamcar@hotmail.com>
2018-11-21 16:50:46 +0100

    Add Slovenian translation

--- src/locales/index.js
+++ src/locales/index.js
@@ -3,9 +3,11 @@
 // ==========================================================================
 
 import en from './en';
+import sl from './sl';
 
 const languages = [
     { code: 'en', name: 'English', i18n: en },
+    { code: 'sl', name: 'Slovenščina', i18n: sl },
 ];
 
 export default languages;
