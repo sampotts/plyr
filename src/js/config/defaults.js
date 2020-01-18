eefdb8dc401011edd3efe81f4f5f4bfe5504d4e7
Filip Š <filip.stamcar@hotmail.com>
2020-01-18 22:58:00 +0100

    Fix merge

--- src/js/config/defaults.js
+++ src/js/config/defaults.js
@@ -145,57 +145,8 @@ const defaults = {
     ],
     settings: ['captions', 'quality', 'speed'],
 
-<<<<<<< HEAD
     // Custom localisation
     i18n: {},
-=======
-    // Localisation
-    i18n: {
-        restart: 'Restart',
-        rewind: 'Rewind {seektime}s',
-        play: 'Play',
-        pause: 'Pause',
-        fastForward: 'Forward {seektime}s',
-        seek: 'Seek',
-        seekLabel: '{currentTime} of {duration}',
-        played: 'Played',
-        buffered: 'Buffered',
-        currentTime: 'Current time',
-        duration: 'Duration',
-        volume: 'Volume',
-        mute: 'Mute',
-        unmute: 'Unmute',
-        enableCaptions: 'Enable captions',
-        disableCaptions: 'Disable captions',
-        download: 'Download',
-        enterFullscreen: 'Enter fullscreen',
-        exitFullscreen: 'Exit fullscreen',
-        frameTitle: 'Player for {title}',
-        captions: 'Captions',
-        settings: 'Settings',
-        pip: 'PIP',
-        menuBack: 'Go back to previous menu',
-        speed: 'Speed',
-        normal: 'Normal',
-        quality: 'Quality',
-        loop: 'Loop',
-        start: 'Start',
-        end: 'End',
-        all: 'All',
-        reset: 'Reset',
-        disabled: 'Disabled',
-        enabled: 'Enabled',
-        advertisement: 'Ad',
-        qualityBadge: {
-            2160: '4K',
-            1440: 'HD',
-            1080: 'HD',
-            720: 'HD',
-            576: 'SD',
-            480: 'SD',
-        },
-    },
->>>>>>> a77d2d56f67d6b601940d89bd8ac9831e0949963
 
     // URLs
     urls: {
