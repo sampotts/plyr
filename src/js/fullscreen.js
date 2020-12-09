b460a4a16ac57172dec84e3c088bf88e67d40aa4
Elias Saalmann <es@zeit.dev>
2020-12-09 16:39:47 +0100

    Fix issue with not entering iosfullscreen of vimeo videos with playsinline=true

--- src/js/fullscreen.js
+++ src/js/fullscreen.js
@@ -254,7 +254,11 @@ class Fullscreen {
 
     // iOS native fullscreen doesn't need the request step
     if (browser.isIos && this.player.config.fullscreen.iosNative) {
-      this.target.webkitEnterFullscreen();
+      if (this.player.provider === 'vimeo') {
+        this.player.embed.requestFullscreen();
+      } else {
+        this.target.webkitEnterFullscreen();
+      }
     } else if (!Fullscreen.native || this.forceFallback) {
       this.toggleFallback(true);
     } else if (!this.prefix) {
